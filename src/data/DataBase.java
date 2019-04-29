75bd5beac861a7786339fbdc6da58b32437be27a
yaziek <yaziek@gmail.com>
2019-04-29 21:57:42 +0200

    Adding DataBase Class
    Data base of all tips + classes:
    add() - add tip

---
+++ src/data/DataBase.java
@@ -0,0 +1,13 @@
+package data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataBase {
+
+    List<Double> tips = new ArrayList<>();
+
+    public void add(Double tip){
+        tips.add(tip);
+    }
+}
